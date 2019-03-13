7ad2d844cabdacffa5fef1e436a49a770f5852e0
rotysz <44545129+rotysz@users.noreply.github.com>
2019-03-13 22:07:28 +0100

    Updates.

--- main.ts
+++ main.ts
@@ -1,7 +1,16 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+
+let ServPos = 1500
+Servo.ServoPulse(0, 1500)
+Servo.ServoPulse(1, 1500)
+
+input.onButtonPressed(Button.A, function () {
+    ServPos = ServPos - 500
+    Servo.ServoPulse(0, ServPos)
+    Servo.ServoPulse(1, ServPos + 1)
+})
+
+input.onButtonPressed(Button.B, function () {
+    ServPos = ServPos + 500
+    Servo.ServoPulse(0, ServPos)
+    Servo.ServoPulse(1, ServPos - 1)
+})
