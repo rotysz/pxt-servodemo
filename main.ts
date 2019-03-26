07fb7dbdc381367aa774fc5b77da167e3256321f
rotysz <44545129+rotysz@users.noreply.github.com>
2019-03-26 23:33:11 +0100

    Updates.

--- main.ts
+++ main.ts
@@ -1,25 +1,40 @@
+const INIT_LEG = 90
+const INIT_KNEE = 25
 
-let ServPos = 90
-Servo.Servo(0, ServPos)
-Servo.Servo(1, ServPos)
-let ActChl = 0
-basic.showNumber(ActChl)
+let ServPos = INIT_KNEE
+let ServoNum = 0
+let ActChl = 6
+for (let Srv = 0; Srv < 6; Srv++) {
+    Servo.Servo(Srv, INIT_LEG)
+    Servo.Servo(Srv + 6, INIT_KNEE)
+}
+
+basic.showString("I")
 
 input.onButtonPressed(Button.A, function () {
     ServPos = ServPos + 5
-    Servo.Servo(ActChl, ServPos)
+    for (let Srv = 0; Srv < 6; Srv++) {
+        //        Servo.Servo(Srv, Se)
+        Servo.Servo(Srv + ActChl, ServPos)
+    }
+    basic.showNumber(ServPos)
 })
 
 input.onButtonPressed(Button.B, function () {
     ServPos = ServPos - 5
-    Servo.Servo(ActChl, ServPos)
+    for (let Srv = 0; Srv < 6; Srv++) {
+        Servo.Servo(Srv + ActChl, ServPos)
+    }
+    basic.showNumber(ServPos)
 })
 
 input.onButtonPressed(Button.AB, function () {
     if (ActChl == 0) {
-        ActChl = 1
+        ActChl = 6
+        ServPos = INIT_KNEE
     } else {
         ActChl = 0
+        ServPos = INIT_LEG
     }
     basic.showNumber(ActChl)
 })
